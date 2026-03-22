1d82828e6fd19bdf413d5a82e89c529494b9ac10
anushasreerama <anushasreerama9@gmail.com>
2026-03-22 13:00:31 +0530

    db config for render

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./hrms.db"   # simple for now
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./hrms.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine)
 
